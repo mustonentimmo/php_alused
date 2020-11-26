e87b5c805bfe5eadafed0a3c3bee4bb0bf1d910e
Timmo <timmo.mustonen@khk.com>
2020-11-26 10:23:09 +0200

    Ülesanne 1

---
+++ php_ÜL1.php
@@ -0,0 +1,18 @@
+<?php
+/*
+	Ülesanne 1
+    Nimi: My name is Mustonen, Timmo Mustonen
+    Kuupäev: 2020.11.24
+*/
+
+    $nimi = "Timmo";
+    $sünniaasta = "2020";
+    $tähtkuju = "neitsi";
+
+    $jänks = "(\ (\ <br> ( -.-) <br> 0_('')('')";
+
+echo '<xmp>'. $nimi .'</xmp>';
+echo '<xmp>'. $sünniaasta . '</xmp>';
+echo '<xmp>'. $tähtkuju .'</xmp>';
+echo $jänks;
+
