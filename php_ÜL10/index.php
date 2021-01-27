787cdae336c41c0f7b1b822a66e1ad754db920be
Timmo <timmo.mustonen@khk.com>
2021-01-27 23:20:05 +0200

    ÜL 10

---
+++ php_ÜL10/index.php
@@ -0,0 +1,12 @@
+<menu>
+    <a href="index.php"></a>
+    <a href="index.php?leht=portfoolio"></a>
+    <a href="index.php?leht=kaart"></a>
+    <a href="index.php?leht=kontakt"></a>
+</menu>
+
+<h1>Index</h1>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan urna id diam dignissim, in interdum nulla viverra. Nullam pharetra ultricies convallis. Nulla nisl ante, laoreet eget dignissim in, aliquam non arcu. Aenean tristique nunc odio, ut sodales eros placerat mattis. Integer elit augue, maximus ut augue vitae, commodo luctus tortor. Donec maximus mollis libero, ut bibendum leo ornare ac. Phasellus placerat malesuada augue, sit amet suscipit velit sagittis et. Maecenas nisi risus, tincidunt vitae nulla eu, varius placerat purus. Etiam mauris dolor, convallis interdum nisl nec, hendrerit vestibulum ex. Ut vel ipsum nulla. Vivamus at orci consequat, vestibulum sapien quis, faucibus felis.</p>
+
+<?php
