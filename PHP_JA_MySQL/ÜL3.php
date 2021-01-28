61ab86f767bcca3c9758a9e4a2ba9bbae3e52deb
Timmo <timmo.mustonen@khk.com>
2021-01-28 13:39:25 +0200

    ül3 ja ül4

--- PHP_JA_MySQL/ÜL3.php
+++ PHP_JA_MySQL/ÜL3.php
@@ -43,7 +43,6 @@ if (!empty($_GET['artist']) && !empty($_GET['album']) && !empty($_GET['aasta']) 
         <tr><td><input type="reset" value="Tühjenda"></td><td><input type="submit" value="LISA ALBUM"></td></tr>
     </table>
 
-
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
 </form>
---
+++ PHP_JA_MySQL/ÜL4.php
@@ -0,0 +1,13 @@
+<?php
+
+// Import DB connection information
+include('config.php');
+
+$paring = ' SELECT klient.id, klient.eesnimi, klient.telenr, arve.arvenr, arve.maksumus FROM klient LEFT JOIN arve ON klient.id = arve.klient_id;';
+$valjund = mysqli_query($yhendus, $paring);
+while($rida = mysqli_fetch_array($valjund)){
+    echo '<strong>Klient: '.$rida[0].' - '.$rida[1].'</strong><br>';
+    echo 'telefoninumber: '.$rida[2].'<br>';
+    echo 'Arvenumber: '.$rida[3].'<br>';
+    echo 'Maksumus: '.$rida[4].'<br>';
+}
